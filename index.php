fcbc2d3bee28a0bb7030ac01d5a4ff854aa2d7bd
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 15:04:56 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $hautDeFrance = array(
+
+          02 => 'Aisne',
+          59 => 'Nord',
+          60 => 'Oise',
+          62 => 'Pas-de-Calais',
+          80 => 'Somme',
+        );
+       ?>
+    </p>
+  </body>
+</html>
